5444fc13595f95735ed12c1aaaf9280aee29b968
sayokzaman <sayok.zaman.sz@gmail.com>
2026-01-12 22:16:12 +0600

    Add Vercel SPA routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import SolarSystem from './pages/solar-system'
 import ScalesPage from './pages/scales-page'
 
 function App() {
+    const basename = import.meta.env.PROD ? '/' : '/'
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <Routes>
                 <Route path='/' element={<SolarSystem />} />
 
